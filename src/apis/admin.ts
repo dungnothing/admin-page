4ba24cac5f79d7222bfbb9026690da77893f94e8
buidung <dungbh@authenticeducationhub.com>
2025-11-20 17:59:26 +0700

    new

--- src/apis/admin.ts
+++ src/apis/admin.ts
@@ -1,11 +1,11 @@
 import axiosInstance from "./index"
 
 export const loginAPI = async (loginData: any) => {
-  const response = await axiosInstance.post("/v1/admins/login", loginData)
+  const response = await axiosInstance.post("/v2/admins/login", loginData)
   return response.data
 }
 
 export const getUserInfoAPI = async () => {
-  const response = await axiosInstance.get("/v1/admins")
+  const response = await axiosInstance.get("/v2/admins")
   return response.data
 }
